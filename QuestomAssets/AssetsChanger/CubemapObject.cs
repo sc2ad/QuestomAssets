b4ee273484f79c2ff60c2d92eb53a6ec1cf4df52
darknight1050 <darkdeveloping1050@gmail.com>
2020-03-20 04:08:08 +0100

    Added Cubemap and fixed TexEnv

---
+++ QuestomAssets/AssetsChanger/CubemapObject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuestomAssets.AssetsChanger
+{
+    public sealed class CubemapObject : TextureObject
+    {
+        public CubemapObject(IObjectInfo<AssetsObject> objectInfo, AssetsReader reader) : base(objectInfo)
+        {
+            Parse(reader);
+        }
+
+        public CubemapObject(AssetsFile assetsFile) : base(assetsFile, AssetsConstants.ClassID.CubemapClassID)
+        { }
+        public override void Parse(AssetsReader reader)
+        {
+            base.Parse(reader);
+            SourceTextures = reader.ReadArrayOf(r => (ISmartPtr<Texture2DObject>)SmartPtr<Texture2DObject>.Read(ObjectInfo.ParentFile, this, r));
+        }
+
+        protected override void WriteObject(AssetsWriter writer)
+        {
+            base.WriteObject(writer);
+            writer.WriteArrayOf(SourceTextures, (o, w) => o.Write(w));
+        }
+
+        public List<ISmartPtr<Texture2DObject>> SourceTextures { get; set; }
+
+    }
+}
--- QuestomAssets/AssetsChanger/TexEnv.cs
+++ QuestomAssets/AssetsChanger/TexEnv.cs
@@ -16,7 +16,7 @@ namespace QuestomAssets.AssetsChanger
 
         public void Parse(AssetsFile file, AssetsObject owner, AssetsReader reader)
         {
-            Texture = SmartPtr<Texture2DObject>.Read(file, owner, reader);
+            Texture = SmartPtr<TextureObject>.Read(file, owner, reader);
             Scale = new Vector2F(reader);
             Offset = new Vector2F(reader);
         }
@@ -29,7 +29,7 @@ namespace QuestomAssets.AssetsChanger
         }
 
         //this is a Texture, I think that means Texture2D?  is there another kind?
-        public SmartPtr<Texture2DObject> Texture { get; set; }
+        public SmartPtr<TextureObject> Texture { get; set; }
         public Vector2F Scale { get; set; }
         public Vector2F Offset { get; set; }
     }
